311f6f4be910b20f3e4f3e8dbe0df638653111da
Airton Canale <airtonluizcanalefilho@hotmail.com>
2024-09-13 18:31:51 -0300

    feat: initialize the project

---
+++ App.java
@@ -0,0 +1,9 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public void executar() {
+        System.out.println("Aplicacao de sistema de controle de aquisições...");
+    }
+
+}
