d2d54cb947f825f7860c349be72e23c41987135b
Airton Canale <airtonluizcanalefilho@hotmail.com>
2024-09-23 20:13:23 -0300

    feat: add classes and structure the project

---
+++ Item.java
@@ -0,0 +1,32 @@
+public class Item {
+    private String descricao;
+    private double valorUnitario;
+    private int quantidade;
+
+    public Item(String descricao, double valorUnitario, int quantidade) {
+        this.descricao = descricao;
+        this.valorUnitario = valorUnitario;
+        this.quantidade = quantidade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getTotalItem() {
+        return valorUnitario * quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return descricao + " - Quantidade: " + quantidade + ", Valor Unitário: " + valorUnitario + ", Total: " + getTotalItem();
+    }
+}
